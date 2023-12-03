7bd4e7b4cc22caa3c9aba3655d08090915d1bb2c
Samuele Guerrini <sguerrini97@gmail.com>
2023-12-03 17:01:34 +0100

    Preparing to solve part 2 of day 3...

--- 03_Gear_Ratios/main.py
+++ 03_Gear_Ratios/main.py
@@ -26,52 +26,72 @@ with open(sys.argv[1]) as f:
 
     def is_symbol(c: str) -> bool:
         return not c.isdigit() and c != "."
+    
+    def get_adjacent_values(i, j):
+        return [
+            # previous row
+            schematics[i - 1][j - 1] if i != 0 and j != 0                       else ".",
+            schematics[i - 1][j]     if i != 0                                  else ".",
+            schematics[i - 1][j + 1] if i != 0 and j != side_len - 1            else ".",
+            # current row
+            schematics[i][j - 1]     if j != 0                                  else ".",
+            schematics[i][j + 1]     if j != side_len - 1                       else ".",
+            # next row
+            schematics[i + 1][j - 1] if i != side_len -1 and j != 0             else ".",
+            schematics[i + 1][j]     if i != side_len - 1                       else ".",
+            schematics[i + 1][j + 1] if i != side_len - 1 and j != side_len - 1 else ".",
+        ]
 
     side_len = len(schematics) # assyume schematics is a square matrix
-    for i in range(0, side_len):
+    
+    # Check for parts numbers
+    if parse_mode == 1:
+        for i in range(0, side_len):
+            digits = ""
+            counts = False
 
-        digits = ""
-        counts = False
+            for j in range(0, side_len):
 
-        for j in range(0, side_len):
+                c = schematics[i][j]
 
-            c = schematics[i][j]
+                # stop on non-digit characters
+                if not c.isdigit():
+                    if counts and len(digits) > 0:
+                        parts_sum += int(digits)
+                    digits = ""
+                    counts = False
+                    continue
 
-            # stop on non-digit characters
-            if not c.isdigit():
-                if counts and len(digits) > 0:
-                    parts_sum += int(digits)
-                digits = ""
-                counts = False
-                continue
+                digits += c
 
-            digits += c
+                # skip if we already know this part counts
+                if counts:
+                    continue
 
-            # skip if we already know this part counts
-            if counts:
-                continue
+                adjacent_values = get_adjacent_values(i, j)
+                adjacent_symbols = list(filter(lambda c: is_symbol(c), adjacent_values))
+                if len(adjacent_symbols) > 0:
+                    counts = True
+                    continue
 
-            adjacent_values = [
-                # previous row
-                schematics[i - 1][j - 1] if i != 0 and j != 0                       else ".",
-                schematics[i - 1][j]     if i != 0                                  else ".",
-                schematics[i - 1][j + 1] if i != 0 and j != side_len - 1            else ".",
-                # current row
-                schematics[i][j - 1]     if j != 0                                  else ".",
-                schematics[i][j + 1]     if j != side_len - 1                       else ".",
-                # next row
-                schematics[i + 1][j - 1] if i != side_len -1 and j != 0             else ".",
-                schematics[i + 1][j]     if i != side_len - 1                       else ".",
-                schematics[i + 1][j + 1] if i != side_len - 1 and j != side_len - 1 else ".",
-            ]
+            if counts and len(digits) > 0:
+                parts_sum += int(digits)
+            
+        print(parts_sum)
+    
+    # Check for gear ratios
+    else:
+        for i in range(0, side_len):
+            digits = ""
+            counts = False
 
-            adjacent_symbols = list(filter(lambda c: is_symbol(c), adjacent_values))
-            if len(adjacent_symbols) > 0:
-                counts = True
-                continue
+            for j in range(0, side_len):
 
-        if counts and len(digits) > 0:
-            parts_sum += int(digits)
+                c = schematics[i][j]
+                if c == "*":
 
-    print(parts_sum)
+                    adjacent_values = get_adjacent_values(i, j)
 
+                    pass
+
+                pass
