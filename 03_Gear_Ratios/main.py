dc8b36b3a510ab3afd8d8f8a68957c1fab0a076e
Samuele Guerrini <sguerrini97@gmail.com>
2023-12-03 12:43:49 +0100

    Added template and input for challenge 3

---
+++ 03_Gear_Ratios/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+# Check for input file argument
+if len(sys.argv) != 3:
+    print(f"Usage: {sys.argv[0]} <input_file> <part>")
+    sys.exit(1)
+
+# Check for part argument
+parse_mode = int(sys.argv[2])
+if parse_mode not in [1, 2]:
+    print(f"Invalid challenge part: {sys.argv[2]}")
+    sys.exit(2)
+
+# Parse input file
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+
+    for line in lines:
+
+        pass
+
+
