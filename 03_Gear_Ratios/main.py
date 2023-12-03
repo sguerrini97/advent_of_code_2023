39b9ef608e9a339b475ec310e1926dd85e014b16
Samuele Guerrini <sguerrini97@gmail.com>
2023-12-03 16:46:34 +0100

    Solved part 1 of challenge 3

--- 03_Gear_Ratios/main.py
+++ 03_Gear_Ratios/main.py
@@ -17,8 +17,61 @@ if parse_mode not in [1, 2]:
 with open(sys.argv[1]) as f:
     lines = f.readlines()
 
-    for line in lines:
+    parts_sum = 0
+    schematics: list[list[str]] = []
 
+    for line in lines:
+        schematics.append(list(line.strip()))
         pass
 
+    def is_symbol(c: str) -> bool:
+        return not c.isdigit() and c != "."
+
+    side_len = len(schematics) # assyume schematics is a square matrix
+    for i in range(0, side_len):
+
+        digits = ""
+        counts = False
+
+        for j in range(0, side_len):
+
+            c = schematics[i][j]
+
+            # stop on non-digit characters
+            if not c.isdigit():
+                if counts and len(digits) > 0:
+                    parts_sum += int(digits)
+                digits = ""
+                counts = False
+                continue
+
+            digits += c
+
+            # skip if we already know this part counts
+            if counts:
+                continue
+
+            adjacent_values = [
+                # previous row
+                schematics[i - 1][j - 1] if i != 0 and j != 0                       else ".",
+                schematics[i - 1][j]     if i != 0                                  else ".",
+                schematics[i - 1][j + 1] if i != 0 and j != side_len - 1            else ".",
+                # current row
+                schematics[i][j - 1]     if j != 0                                  else ".",
+                schematics[i][j + 1]     if j != side_len - 1                       else ".",
+                # next row
+                schematics[i + 1][j - 1] if i != side_len -1 and j != 0             else ".",
+                schematics[i + 1][j]     if i != side_len - 1                       else ".",
+                schematics[i + 1][j + 1] if i != side_len - 1 and j != side_len - 1 else ".",
+            ]
+
+            adjacent_symbols = list(filter(lambda c: is_symbol(c), adjacent_values))
+            if len(adjacent_symbols) > 0:
+                counts = True
+                continue
+
+        if counts and len(digits) > 0:
+            parts_sum += int(digits)
+
+    print(parts_sum)
 
